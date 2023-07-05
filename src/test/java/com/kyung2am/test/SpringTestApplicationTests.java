f14c9c38df715e1e851c27e28441848aee5c1bdc
김경남 <lietty@naver.com>
2023-07-05 20:05:15 +0900

    [김경남] Spring Test 최초파일

---
+++ src/test/java/com/kyung2am/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyung2am.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
